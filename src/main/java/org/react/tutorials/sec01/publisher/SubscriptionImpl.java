4ad24d47625f1a325c71eab5c64a59532e1852a7
Reggie <reggie.senal@gmail.com>
2024-10-29 02:41:08 +0800

    rest of the code

---
+++ src/main/java/org/react/tutorials/sec01/publisher/SubscriptionImpl.java
@@ -0,0 +1,49 @@
+package org.react.tutorials.sec01.publisher;
+
+import com.github.javafaker.Faker;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubscriptionImpl implements Subscription {
+
+    private static final Logger log = LoggerFactory.getLogger(SubscriptionImpl.class);
+    private static final int MAX_ITEMS = 10;
+    private final Faker faker;
+    private final Subscriber<? super String> subscriber;
+    private boolean isCancelled;
+    private int count = 0;
+
+    public SubscriptionImpl(Subscriber<? super String> subscriber) {
+        this.subscriber = subscriber;
+        this.faker = Faker.instance();
+    }
+
+    @Override
+    public void request(long requested) {
+        if (isCancelled) {
+            return;
+        }
+        log.info("subscriber has requested {} items", requested);
+        if (requested > MAX_ITEMS){
+            this.subscriber.onError(new RuntimeException("request is over max item limit."));
+            this.isCancelled = true; // stops going to the onNext and end exits
+            return;
+        }
+        for (int i = 0; i < requested && count < MAX_ITEMS; i++){
+            count++;
+            this.subscriber.onNext(this.faker.internet().emailAddress());
+        }
+        if (count == MAX_ITEMS){
+            log.info("no more data to produce");
+            this.subscriber.onComplete();
+        }
+    }
+
+    @Override
+    public void cancel() {
+        log.info("subscriber has cancelled");
+        this.isCancelled = true;
+    }
+}
