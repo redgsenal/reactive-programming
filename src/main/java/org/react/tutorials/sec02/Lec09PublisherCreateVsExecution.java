38b713bcb2db520ba8470ace7a5057beeab65f1f
redgsenal <reggie.senal@gmail.com>
2024-11-10 21:14:55 +0800

    fromSupplier

---
+++ src/main/java/org/react/tutorials/sec02/Lec09PublisherCreateVsExecution.java
@@ -0,0 +1,26 @@
+package org.react.tutorials.sec02;
+
+import org.react.tutorials.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public class Lec09PublisherCreateVsExecution {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec09PublisherCreateVsExecution.class);
+
+    public static void main(String[] args) {
+        getName()
+                .subscribe(Util.subscriber());
+    }
+
+    private static Mono<String> getName() {
+        log.info("wait to generate a name...");
+        return Mono.fromSupplier(() -> {
+            log.info("generating a name...");
+            // simulate a long process
+            Util.sleepSeconds(5);
+            return Util.faker().name().fullName();
+        });
+    }
+}
