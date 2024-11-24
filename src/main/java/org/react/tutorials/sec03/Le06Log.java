a8acb09d1952292b5d286573605ce59186e0ff59
Reggie <reggie.senal@gmail.com>
2024-11-25 00:17:42 +0800

    log operator

---
+++ src/main/java/org/react/tutorials/sec03/Le06Log.java
@@ -0,0 +1,15 @@
+package org.react.tutorials.sec03;
+
+import org.react.tutorials.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Le06Log {
+
+    public static void main(String[] args) {
+        Flux.range(1, 100)
+                .log("numbers ->")
+                .map(i -> Util.faker().name().fullName())
+                .log("names ->")
+                .subscribe(Util.subscriber());
+    }
+}
