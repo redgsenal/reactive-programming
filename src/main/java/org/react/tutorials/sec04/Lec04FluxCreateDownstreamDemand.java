febd17bbd30491716d7f87ad67945e21e2d3a6a3
Reggie <reggie.senal@gmail.com>
2024-12-26 04:28:19 +0800

    flux sink thread safety request on demand

--- src/main/java/org/react/tutorials/sec04/Lec04FluxCreateDownstreamDemand.java
+++ src/main/java/org/react/tutorials/sec04/Lec04FluxCreateDownstreamDemand.java
@@ -10,6 +10,10 @@ public class Lec04FluxCreateDownstreamDemand {
     private static final Logger log = LoggerFactory.getLogger(Lec04FluxCreateDownstreamDemand.class);
 
     public static void main(String[] args) {
+        produceOnDemand();
+    }
+
+    private static void produceEarly(){
         var subscriber = new SubscriberImpl();
         Flux.<String>create(fluxSink -> {
             for (int i = 0; i < 15; i++) {
@@ -28,6 +32,27 @@ public class Lec04FluxCreateDownstreamDemand {
         subscriber.getSubscription().cancel();
         // this part will not request
         subscriber.getSubscription().request(3);
+    }
 
+    private static void produceOnDemand(){
+        var subscriber = new SubscriberImpl();
+        Flux.<String>create(fluxSink -> {
+            fluxSink.onRequest(requestValue -> {
+                for (int i = 0; i < requestValue; i++) {
+                    var name = Util.faker().name().fullName();
+                    log.info("generated name: {}", name);
+                    fluxSink.next(name);
+                }
+            });
+        }).subscribe(subscriber);
+
+        // do cancel next
+        subscriber.getSubscription().request(3);
+        Util.sleepSeconds(1);
+        subscriber.getSubscription().request(3);
+        Util.sleepSeconds(1);
+        subscriber.getSubscription().cancel();
+        // this part will not request
+        subscriber.getSubscription().request(3);
     }
 }
