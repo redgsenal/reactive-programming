c18ff105c3fe24f4ad1477857c9186a75b7b4b79
Reggie <reggie.senal@gmail.com>
2025-01-01 08:16:02 +0800

    lecture 05 take opeerators

--- src/main/java/org/react/tutorials/sec04/Lec05TakeOperator.java
+++ src/main/java/org/react/tutorials/sec04/Lec05TakeOperator.java
@@ -12,21 +12,38 @@ public class Lec05TakeOperator {
     private static final Logger log = LoggerFactory.getLogger(Lec05TakeOperator.class);
 
     public static void main(String[] args) {
-        fluxRange();
+        fluxRangeTakeUntil();
     }
 
     private static void intStream() {
         IntStream.rangeClosed(1, 10)
-                .limit(5)
+                .limit(5) // loop until limit of 5 items
                 .forEach(value -> {
                     log.info("value: {}", value);
                 });
     }
 
-    private static void fluxRange() {
+    private static void fluxRangeTake() {
         Flux.range(1, 10)
                 .log("take: ")
-                .take(7)
+                .take(7) // take 7 items from a range of 1 to 10
+                .log("subscribe: ")
+                .subscribe(Util.subscriber());
+    }
+
+    private static void fluxRangeTakeWhile() {
+        Flux.range(1, 100)
+                .log("take: ")
+                .takeWhile(v -> v < 30) // loop while value less than 30
+                .log("subscribe: ")
+                .subscribe(Util.subscriber());
+    }
+
+
+    private static void fluxRangeTakeUntil() {
+        Flux.range(1, 100)
+                .log("take: ")
+                .takeUntil(v -> v == 20) // loop until v == 20
                 .log("subscribe: ")
                 .subscribe(Util.subscriber());
     }
