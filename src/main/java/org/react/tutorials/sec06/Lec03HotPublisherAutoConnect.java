c511dce8e349b4e396552c15f077699fab4e9248
Reggie Senal <reggie.senal@gmail.com>
2025-11-04 06:14:21 +0800

    update

--- src/main/java/org/react/tutorials/sec06/Lec03HotPublisherAutoConnect.java
+++ src/main/java/org/react/tutorials/sec06/Lec03HotPublisherAutoConnect.java
@@ -4,9 +4,9 @@ import org.react.tutorials.common.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Lec03AutoConnect {
+public class Lec03HotPublisherAutoConnect {
 
-    private static final Logger log = LoggerFactory.getLogger(Lec03AutoConnect.class);
+    private static final Logger log = LoggerFactory.getLogger(Lec03HotPublisherAutoConnect.class);
 
     public static void main(String[] args) {
         log.info("Starting Lec03AutoConnect...");
